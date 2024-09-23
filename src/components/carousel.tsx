53f8a9129fcf0d879f018e1c5d859913e779eb6e
dzakyadlh <106021129+dzakyadlh@users.noreply.github.com>
2024-09-23 23:52:54 +0700

    initial push

---
+++ src/components/carousel.tsx
@@ -0,0 +1,7 @@
+export default function Carousel() {
+  return (
+    <div className="w-2/5 h-1/5 rounded-md">
+      <div></div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        'poppins-light': ['Poppins Light', 'sans-serif'],
+        'poppins-regular': ['Poppins Regular', 'sans-serif'],
+        'poppins-medium': ['Poppins Medium', 'sans-serif'],
+        'poppins-semibold': ['Poppins Semibold', 'sans-serif'],
+        'poppins-bold': ['Poppins Bold', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom-black': '4px 4px 0px black', // Customize the shadow here
       },
     },
   },
